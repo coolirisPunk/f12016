25f0525359d87ac89f72394ca8ba20a029cc7478
DaniPunk <german@punkmkt.com>
2016-07-20 10:17:32 -0500

    Api v1

--- formula1/settings/production.py
+++ formula1/settings/production.py
@@ -5,14 +5,21 @@ ALLOWED_HOSTS = ['*']
 
 TEMPLATE_DEBUG = DEBUG
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': get_secret('DATABASE_NAME'),
+#         'USER': get_secret('DATABASE_USER'),
+#         'PASSWORD': get_secret('DATABASE_PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_secret('DATABASE_NAME'),
-        'USER': get_secret('DATABASE_USER'),
-        'PASSWORD': get_secret('DATABASE_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': normpath(join(SITE_ROOT, "db.sqlite3")),
     }
 }
 # ######### EMAIL CONFIGURATION
