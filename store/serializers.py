f7c00e757882d733be7fcd0c635c856c1ad71bb5
germanpunk <german@punkmkt.com>
2016-08-15 10:29:13 -0500

    Absolute url store field picture

--- store/serializers.py
+++ store/serializers.py
@@ -1,29 +1,43 @@
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, serializers
 from .models import *
 from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 from common.serializers import DynamicFieldsModelSerializer
+from django.contrib.sites.models import Site
+from django.conf import settings
+damain_url = Site.objects.get_current().domain
 
 
 class CategoryProductSerializer(DynamicFieldsModelSerializer):
+    picture = serializers.SerializerMethodField('get_picture_url')
     class Meta:
         model = CategoryProduct
         fields = [
             'id', 'name', 'picture',
         ]
 
+    def get_picture_url(self, obj):
+        return '%s%s%s' % (damain_url, settings.MEDIA_URL, obj.picture)
+
 
 class SellerSerializer(DynamicFieldsModelSerializer):
+    picture = serializers.SerializerMethodField('get_picture_url')
     class Meta:
         model = Seller
         fields = [
             'id', 'name', 'picture',
         ]
 
+    def get_picture_url(self, obj):
+        return '%s%s%s' % (damain_url, settings.MEDIA_URL, obj.picture)
 
 class ProductSerializer(DynamicFieldsModelSerializer):
+    picture = serializers.SerializerMethodField('get_picture_url')
     class Meta:
         model = Product
         fields = [
             'id', 'name', "picture" ,'description', 'price','review',
         ]
+
+    def get_picture_url(self, obj):
+        return '%s%s%s' % (damain_url, settings.MEDIA_URL, obj.picture)
