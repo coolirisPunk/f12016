975b8e959887f6faab53712c6b56d8cf6fe77e7f
germanpunk <german@punkmkt.com>
2016-09-05 12:25:21 -0500

    swgi

--- formula1/settings/base.py
+++ formula1/settings/base.py
@@ -151,8 +151,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
 STATIC_ROOT = normpath(join(BASE_DIR, 'assets'))
+print STATIC_ROOT
 
 STATIC_URL = '/static/'
 
