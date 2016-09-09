d1ee26407d7706a6faaa7fa0b777740a57fc9c8b
germanpunk <german@punkmkt.com>
2016-09-09 18:48:47 -0500

    model premio

--- premio/admin.py
+++ premio/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class SectionResource(resources.ModelResource):
+
+    class Meta:
+        model = Section
+        import_id_fields = ['id']
 
 admin.site.register(EventDay)
 admin.site.register(EventType)
@@ -22,3 +31,15 @@ admin.site.register(Hotel)
 admin.site.register(Restaurant)
 admin.site.register(Place)
 admin.site.register(Formula1Taste)
+
+admin.site.register(Zone)
+admin.site.register(Grandstand)
+admin.site.register(Row)
+admin.site.register(Seat)
+
+
+class SectionAdmin(ImportExportModelAdmin):
+	pass
+
+
+admin.site.register(Section, SectionAdmin)
