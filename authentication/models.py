6d4c7da37b37a7fa04783aa9c405b811b5d30132
germanpunk <german@punkmkt.com>
2016-09-14 11:52:10 -0500

    models premio

--- authentication/models.py
+++ authentication/models.py
@@ -9,8 +9,8 @@ from django.conf.urls.static import static
 class UserProfile(models.Model):
     user = models.OneToOneField(User,related_name='user_profile')
     # custom fields for user
-    zone = models.CharField(max_length=10,null=True, blank=True)
-    grada = models.CharField(max_length=10,null=True, blank=True)
+    zone = models.CharField(max_length=100,null=True, blank=True)
+    grada = models.CharField(max_length=100,null=True, blank=True)
     section = models.CharField(max_length=10,null=True, blank=True)
     fila = models.CharField(max_length=10,null=True, blank=True)
     seat = models.CharField(max_length=10,null=True, blank=True)
