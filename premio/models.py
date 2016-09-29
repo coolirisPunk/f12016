bf25ecd0e739eeb57d4690eb1c8c60b7507185dd
coolirisroto <gmtz.1991@gmail.com>
2016-09-29 16:51:29 -0500

    Push notifications

--- premio/models.py
+++ premio/models.py
@@ -50,7 +50,7 @@ class Event(TimeStampModel):
     #event_day = models.ForeignKey(EventDay)
     event_type = models.ForeignKey(EventType, related_name='events')
     ordering = models.IntegerField(default=0)
-    slug = AutoSlugField(unique=True, populate_from='description', null=True, max_length=160)
+    slug = AutoSlugField(unique_with=['description', 'event_type__event_day__description','start_time'], populate_from='description', null=True, max_length=160)
 
     class Meta:
         ordering = ['ordering']
