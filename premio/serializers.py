0787edd61150aef4c8c986e64a1e9c46c3146266
coolirisroto <gmtz.1991@gmail.com>
2016-10-03 16:43:33 -0500

    date field event

--- premio/serializers.py
+++ premio/serializers.py
@@ -13,7 +13,7 @@ class EventSerializer(DynamicFieldsModelSerializer):
     class Meta:
         model = Event
         fields = [
-            'id', 'description', 'start_time', 'ordering','slug'
+            'id', 'description', 'start_time', 'ordering','slug','date'
         ]
         depth = 1
         order_by = (('ordering',))
