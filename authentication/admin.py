28fe0a956d13620d36e24f3f59815ac7ba5f34a2
coolirisroto <gmtz.1991@gmail.com>
2016-10-19 13:25:45 -0500

    email server

--- authentication/admin.py
+++ authentication/admin.py
@@ -10,8 +10,13 @@ class UserProfileInline(admin.TabularInline):
 class UserAdmin(admin.ModelAdmin):
 	model = User
 	inlines = [UserProfileInline,]
+	list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined')
+	list_filter = ('is_staff', 'is_superuser')
 
 
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 class UserProfileAdmin(admin.ModelAdmin):
 	model = UserProfile
@@ -19,7 +24,3 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 #admin.site.unregister(User)
 admin.site.register(UserProfile, UserProfileAdmin)
-
-
-
-
